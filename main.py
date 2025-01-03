b17f44aa386ccd8b1a307d4d8836816c48d53a94
Tyler Johnson <tylerjohnson@tylers-mbp-7.lan>
2025-01-02 21:20:16 -0600

    added data file

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import numpy as np
+import sys 
 
 def main():
 	
 	#This is the main function for the TRACE codebase 
  
-	print("hello world")
+	# The command line prompt should be in the form:
+	# python3 main.py <config_file.json> <data_file.dat>
+ 
+ 
+	# parse the command line argument
+ 
+	args = sys.argv[1:]
+ 
+	if len(args) != 2:
+     
+		print("Usage: python3 main.py <config_file.json> <data_file.dat>")
+		sys.exit(1)
+  
+	config_file = args[0]
+ 
+	data_file = args[1]
+ 
+ 
+ 
+	# Load the configuration file json 
 	
 if __name__ == "__main__":
 	main()
