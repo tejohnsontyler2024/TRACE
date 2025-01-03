c7872e39846526badf52dd9848d9d695bd21db60
Tyler Johnson <tylerjohnson@tylers-mbp-7.lan>
2025-01-02 21:53:45 -0600

    adding to the main.py, utils.py, config

--- main.py
+++ main.py
@@ -1,30 +1,66 @@
 import numpy as np
 import sys 
 
+# from src.utils import *
+from src.utils import read_config_file, decode_binary
+
 def main():
 	
 	#This is the main function for the TRACE codebase 
  
 	# The command line prompt should be in the form:
-	# python3 main.py <config_file.json> <data_file.dat>
+	# python3 main.py <config_file.json> 
  
  
 	# parse the command line argument
  
 	args = sys.argv[1:]
  
-	if len(args) != 2:
+	if len(args) != 1:
      
-		print("Usage: python3 main.py <config_file.json> <data_file.dat>")
+		print("Usage: python3 main.py <config_file.json>")
 		sys.exit(1)
   
 	config_file = args[0]
  
-	data_file = args[1]
- 
+	print("Reading configuration file: ", config_file)
  
  
 	# Load the configuration file json 
+ 
+	config = read_config_file(config_file)
+ 
+	data_file_name = config['DATA_FILE']
+	waveform_length = config['WAVEFORM_LENGTH']
+	processes = config['PROCESSES']
+ 
+	binary_parser_bool = processes['BINARY_PARSER']
+	make_toy_waveform_bool = processes['MAKE_TOY_WAVEFORMS']
+ 
+	print("Reading data file: ", data_file_name)
+	print("Waveform length: ", waveform_length)
+ 
+	# now we will decode the binary file. 
+	# Rather than keep in memory, it will be saved as a numpy file.
+ 
+	if binary_parser_bool:
+		output_file_name = decode_binary(data_file_name, waveform_length)
+  
+  
+  
+	# now we will make a toy waveform if the user wants to.
+	if make_toy_waveform_bool:
+		print("Making toy waveforms")
+  
+		# get the toy waveform parameters from the config file
+  
+		toy_waveform_params = config['TOY_WAVEFORM_PARAMS']
+  
+		num_noise_waveforms = toy_waveform_params['NUM_NOISE_WAVEFORMS']
+		num_signal_waveforms = toy_waveform_params['NUM_SIGNAL_WAVEFORMS']
+  
+  
+		# make_toy_waveform()
 	
 if __name__ == "__main__":
 	main()
---
+++ src/utils.py
@@ -0,0 +1,54 @@
+# utils.py
+import json
+import numpy as np
+
+def read_config_file(config_file_name):
+    
+    with open(config_file_name, 'r') as f:
+        config = json.load(f)
+        
+    return config 
+
+def get_path(config_file_name):
+    
+    config_path_split = config_file_name.split('/')[:-1]
+        
+    config_path = ''
+    
+    for item in config_path_split:
+        config_path += item + '/'
+    
+    return config_path
+
+def decode_binary(binary_file, waveform_length):
+    
+    config_path = get_path(binary_file)
+    
+    output_file_name = config_path + 'decoded_waveforms.npy'
+    
+    waveform_length = int(waveform_length)
+    
+    data = np.fromfile(binary_file, dtype=np.int16)
+    
+    length_of_data = len(data)
+        
+    num_waveforms = length_of_data // waveform_length
+    
+    waveforms = np.zeros((num_waveforms, waveform_length))
+
+    for i in range(num_waveforms):
+        
+        index_start = int(i*waveform_length)
+        index_end = int((i+1)*waveform_length)
+        
+        waveforms[i] = -1*data[index_start:index_end]
+    
+    # save the waveforms as a numpy file
+    
+    number_of_waveforms = len(waveforms)
+    
+    print("Number of waveforms: ", number_of_waveforms)
+    
+    np.save(output_file_name, waveforms)
+    
+    return output_file_name
